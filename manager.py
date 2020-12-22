5f8867e37935eabefa42ac81203a9a7ad6a5a8b5
niuyb <niuyubin@yqzbw.com>
2020-12-22 11:45:46 +0800

    test

--- manager.py
+++ manager.py
@@ -1,20 +1,30 @@
-from flask_script import Manager
-from markupsafe import escape
-from cloudcc_api import create_app
+from flask import Flask
+# from flask_script import Manager
+# from markupsafe import escape
+# from cloudcc_api import create_app
+#
+#
+# # flask
+# # app = Flask(__name__)
+# app = create_app()
+#
+# # flask script
+# manager = Manager(app=app)
+#
+#
+#
+# if __name__ == '__main__':
+#     # flask
+#     # app.run(debug=True,port=8000,host="127.0.0.1",threaded=True)
+#
+#     # flask script
+#     manager.run()
 
-
-# flask
-# app = Flask(__name__)
-app = create_app()
-
-# flask script
-manager = Manager(app=app)
-
-
+app = Flask(__name__)
+@app.route("/hello")
+def hello():
+    return "hello"
 
 if __name__ == '__main__':
     # flask
-    # app.run(debug=True,port=8000,host="127.0.0.1",threaded=True)
-
-    # flask script
-    manager.run()
+    app.run(debug=True,port=8000,host="0.0.0.0",threaded=True)
